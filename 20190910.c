e61ae35a2cab2849e6c0fac8b985880abb21c3bb
hayoungzzzzz <jhy901@naver.com>
2019-09-10 12:48:00 +0900

    나) 문제코드 . 가) 문제코드를 수정하여 만듬

--- 20190910.c
+++ 20190910.c
@@ -2,14 +2,28 @@
 
 int main(void)
 {
-
 	int num[10];
 	printf("난수 발생:");
+
+	srand((unsigned)time(NULL));
+
 	for (int i = 0; i < 10; i++)
 	{
 		num[i] = rand() % 10 + 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (num[i] == num[j])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++)
+	{
 		printf("%d ", num[i]);
 	}
 	printf("\n");
 
 }
+
